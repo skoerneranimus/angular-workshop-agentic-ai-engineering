dcec7e906db78809f4523be8930a507c7692e74b
Gregor Woiwode <gregor.woiwode@gmail.com>
2025-09-08 21:27:45 +0200

    solution--prompt-to-feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
+import { BookDetailComponent } from './books/book-detail.component';
 import { BookListComponent } from './books/book-list.component';
 
 export const routes: Routes = [
   { path: '', component: BookListComponent },
+  { path: 'book/:id', component: BookDetailComponent },
   { path: '**', redirectTo: '' }
 ];
